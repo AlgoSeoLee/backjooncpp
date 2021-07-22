096a886299b33639611e2e3d7ea87fc39d986b8b
Hwang-chang-hoon <49145440+Hwang-chang-hoon@users.noreply.github.com>
2021-07-22 21:47:27 +0900

    commit

---
+++ Project1/2553/2553.cpp
@@ -0,0 +1,24 @@
+//https://www.acmicpc.net/problem/2553
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+long long n, ans;
+
+int main()
+{
+	cin >> n;
+	ans = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		ans *= i;
+		ans %= 10000000; // 1Ãµ¸¸
+		while (ans % 10 == 0)
+			ans /= 10;
+	}
+	cout << ans % 10 << '\n';
+
+
+}
