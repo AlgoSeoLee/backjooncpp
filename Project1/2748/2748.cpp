ac1c62ca257ade1d30f59d4d499a848ddb55bc74
Hwang-chang-hoon <49145440+Hwang-chang-hoon@users.noreply.github.com>
2021-08-17 20:45:33 +0900

    commit

---
+++ Project1/2748/2748.cpp
@@ -0,0 +1,22 @@
+//https://www.acmicpc.net/problem/2748
+
+#include <iostream>
+
+using namespace std;
+
+long long fiboarr[100] = { 0,1, };
+
+long long fibo(int N)
+{
+    if (N == 0 || N == 1)
+        return fiboarr[N];
+    else if (fiboarr[N] == 0)
+        fiboarr[N] = fibo(N - 1) + fibo(N - 2);
+    return fiboarr[N];
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << fibo(N);
+}
